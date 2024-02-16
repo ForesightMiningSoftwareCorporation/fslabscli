ccb078075485ebd3ccf13749fadc3b5907a9e822
Loïs Postula <lois@postu.la>
2024-02-16 20:20:07 +0100

    fix: only uses normal deps

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,12 @@ mod utils;
 
 #[derive(Debug, Parser)] // requires `derive` feature
 #[command(
-author,
-version,
-about,
-bin_name("fslabsci"),
-subcommand_required(true),
-propagate_version(true)
+    author,
+    version,
+    about,
+    bin_name("fslabsci"),
+    subcommand_required(true),
+    propagate_version(true)
 )]
 struct Cli {
     /// Enables verbose logging
@@ -29,14 +29,7 @@ struct Cli {
     verbose: u8,
     #[arg(long, global = true)]
     json: bool,
-    #[arg(
-    short,
-    long,
-    global = true,
-    default_value = ".",
-    required = false
-
-    )]
+    #[arg(short, long, global = true, default_value = ".", required = false)]
     working_directory: PathBuf,
     #[command(subcommand)]
     command: Commands,
