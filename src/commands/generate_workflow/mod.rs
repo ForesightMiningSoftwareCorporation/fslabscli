6874377366c0285589ef4ec2741cb4a3d1bfc1d2
Loïs Postula <lois@postu.la>
2024-03-28 14:47:19 +0100

    fix: npmrc is at thome

--- src/commands/generate_workflow/mod.rs
+++ src/commands/generate_workflow/mod.rs
@@ -487,8 +487,8 @@ pub async fn generate_workflow(
                     let github_secret_key = scope.clone().replace('.', "_").to_ascii_uppercase();
                     let run = format!(
                         r#"
-echo "@{scope}:registry=https://npm.pkg.github.com/" >> .npmrc
-echo "//npm.pkg.github.com/:_authToken=${{{{ secrets.NPM_{github_secret_key}_TOKEN }}}}" >> .npmrc
+echo "@{scope}:registry=https://npm.pkg.github.com/" >> ~/.npmrc
+echo "//npm.pkg.github.com/:_authToken=${{{{ secrets.NPM_{github_secret_key}_TOKEN }}}}" >> ~/.npmrc
                     "#
                     );
                     return Some(GithubWorkflowJobSteps {
