d1d8ed880a51724cc19175fc57ea48c1ea7687e7
Loïs Postula <lois@postu.la>
2025-03-19 18:20:44 +0100

    feat: simplify and update nix build

--- flake.nix
+++ flake.nix
@@ -1,18 +1,13 @@
 {
   description = "fslabscli";
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     fenix.url = "github:nix-community/fenix";
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     gitignore.url = "github:hercules-ci/gitignore.nix";
     devenv.url = "github:cachix/devenv";
     treefmt-nix.url = "github:numtide/treefmt-nix";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
   };
   outputs =
     inputs@{
@@ -24,47 +19,64 @@
       devenv,
       gitignore,
       treefmt-nix,
-      rust-overlay,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
         inherit (pkgs.stdenv) isDarwin;
         inherit (gitignore.lib) gitignoreSource;
+        lib = pkgs.lib;
         fenixPkgs = fenix.packages.${system};
-        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        toolchain = fenixPkgs.combine [
+          fenixPkgs.stable.rustc
+          fenixPkgs.stable.cargo
+        ];
         naersk' = pkgs.callPackage naersk {
-          cargo = toolchain;
           rustc = toolchain;
+          cargo = toolchain;
         };
         manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 
         rustSrc = gitignoreSource ./.;
 
-        # Map from architecture name to rust targets and nixpkgs targets.
-        arch2targets = {
-          "x86_64-linux" = {
-            rustTarget = "x86_64-unknown-linux-musl";
-            crossTarget = "x86_64-unknown-linux-musl";
+        arch2targets =
+          let
+            generateCross =
+              target:
+              import nixpkgs {
+                system = system;
+                crossSystem.config = target;
+              };
+          in
+          {
+            "x86_64-linux" = rec {
+              rustTarget = "x86_64-unknown-linux-musl";
+              pkgsCross = generateCross rustTarget;
+              depsBuildBuild = [ ];
+            };
+            "aarch64-darwin" = rec {
+              rustTarget = "aarch64-apple-darwin";
+              pkgsCross = generateCross rustTarget;
+              depsBuildBuild = [ ];
+            };
+            "x86_64-windows" =
+              let
+                pkgsCross = pkgs.pkgsCross.mingwW64;
+              in
+              {
+                inherit pkgsCross;
+                rustTarget = "x86_64-pc-windows-gnu";
+                depsBuildBuild = [
+                  pkgsCross.stdenv.cc
+                  pkgsCross.windows.pthreads
+                ];
+              };
           };
-          "aarch64-linux" = {
-            rustTarget = "aarch64-unknown-linux-musl";
-            crossTarget = "aarch64-unknown-linux-musl";
-          };
-          "i686-linux" = {
-            rustTarget = "i686-unknown-linux-musl";
-            crossTarget = "i686-unknown-linux-musl";
-          };
-          "aarch64-darwin" = {
-            rustTarget = "aarch64-apple-darwin";
-            crossTarget = "aarch64-darwin";
-          };
-        };
         mkRustPackage =
           packageName:
           naersk'.buildPackage {
@@ -87,166 +99,35 @@
             ];
             auditable = false; # Avoid cargo-auditable failures.
             doCheck = false; # Disable test as it requires network access.
+            postInstall =
+              let
+                rustTarget = arch2targets.${system}.rustTarget;
+              in
+              ''
+                $out/bin/${packageName} man-page > ${packageName}.man
+                installManPage ${packageName}.man
+                installShellCompletion --cmd ${packageName} \
+                 --bash <($out/bin/${packageName} completions bash) \
+                 --fish <($out/bin/${packageName} completions fish) \
+                 --zsh <($out/bin/${packageName} completions zsh)
 
-            postInstall = ''
-              $out/bin/${packageName} man-page > ${packageName}.man
-              installManPage ${packageName}.man
-              installShellCompletion --cmd ${packageName} \
-               --bash <($out/bin/${packageName} completions bash) \
-               --fish <($out/bin/${packageName} completions fish) \
-               --zsh <($out/bin/${packageName} completions zsh)
-            '';
-          };
-        pkgsWin64 = pkgs.pkgsCross.mingwW64;
-        mkWin64RustPackage =
-          packageName:
-          let
-            rustTarget = "x86_64-pc-windows-gnu";
-            toolchainWin = fenixPkgs.combine [
-              fenixPkgs.stable.rustc
-              fenixPkgs.stable.cargo
-              fenixPkgs.targets.${rustTarget}.stable.rust-std
-            ];
-            naerskWin = pkgs.callPackage naersk {
-              cargo = toolchainWin;
-              rustc = toolchainWin;
-            };
-          in
-          naerskWin.buildPackage rec {
-            pname = packageName;
-            cargoBuildOptions =
-              x:
-              x
-              ++ [
-                "--package"
-                packageName
-              ];
-            version = manifest.version;
-            strictDeps = true;
-            src = pkgs.lib.cleanSource ./.;
-            nativeBuildInputs = [
-              pkgs.perl # Needed to build vendored OpenSSL.
-            ];
-            depsBuildBuild = [
-              pkgsWin64.stdenv.cc
-              pkgsWin64.windows.pthreads
-            ];
-            auditable = false; # Avoid cargo-auditable failures.
-            doCheck = false; # Disable test as it requires network access.
-            postInstall = ''
-              cd "$out"/bin
-              for f in "$(ls)"; do
-                if ext="$(echo "$f" | grep -oP '\.[a-z]+$')"; then
-                  base="$(echo "$f" | cut -d. -f1)"
-                  mv "$f" "$base-${rustTarget}$ext"
-                else
-                  mv "$f" "$f-${rustTarget}"
-                fi
-              done
-            '';
-            CARGO_BUILD_TARGET = rustTarget;
-            TARGET_CC = "${pkgsWin64.stdenv.cc}/bin/${pkgsWin64.stdenv.cc.targetPrefix}cc";
-            CARGO_BUILD_RUSTFLAGS = [
-              "-C"
-              "linker=${TARGET_CC}"
-            ];
+                cd "$out"/bin
+                for f in "$(ls)"; do
+                  if ext="$(echo "$f" | grep -oP '\.[a-z]+$')"; then
+                    base="$(echo "$f" | cut -d. -f1)"
+                    cp "$f" "$base-${rustTarget}$ext"
+                  else
+                    cp "$f" "$f-${rustTarget}"
+                  fi
+                done
+              '';
 
-            CC = "${pkgsWin64.stdenv.cc}/bin/${pkgsWin64.stdenv.cc.targetPrefix}cc";
-            LD = "${pkgsWin64.stdenv.cc}/bin/${pkgsWin64.stdenv.cc.targetPrefix}cc";
-          };
-
-        pkgsWin32 = pkgs.pkgsCross.mingw32;
-        mkWin32RustPackage =
-          packageName:
-          let
-            rustTarget = "i686-pc-windows-gnu";
-          in
-          let
-            toolchainWin = fenixPkgs.combine [
-              fenixPkgs.stable.rustc
-              fenixPkgs.stable.cargo
-              fenixPkgs.targets.${rustTarget}.stable.rust-std
-            ];
-            naerskWin = pkgs.callPackage naersk {
-              cargo = toolchainWin;
-              rustc = toolchainWin;
-            };
-
-            # Get rid of MCF Gthread library.
-            # See <https://github.com/NixOS/nixpkgs/issues/156343>
-            # and <https://discourse.nixos.org/t/statically-linked-mingw-binaries/38395>
-            # for details.
-            #
-            # Use DWARF-2 instead of SJLJ for exception handling.
-            winCC = pkgsWin32.buildPackages.wrapCC (
-              (pkgsWin32.buildPackages.gcc-unwrapped.override {
-                threadsCross = {
-                  model = "win32";
-                  package = null;
-                };
-              }).overrideAttrs
-                (oldAttr: {
-                  configureFlags = oldAttr.configureFlags ++ [
-                    "--disable-sjlj-exceptions --with-dwarf2"
-                  ];
-                })
-            );
-          in
-          naerskWin.buildPackage rec {
-            pname = packageName;
-            cargoBuildOptions =
-              x:
-              x
-              ++ [
-                "--package"
-                packageName
-              ];
-            version = manifest.version;
-            strictDeps = true;
-            src = pkgs.lib.cleanSource ./.;
-            nativeBuildInputs = [
-              pkgs.perl # Needed to build vendored OpenSSL.
-            ];
-            depsBuildBuild = [
-              winCC
-              pkgsWin32.windows.pthreads
-            ];
-            auditable = false; # Avoid cargo-auditable failures.
-            doCheck = false; # Disable test as it requires network access.
-
-            CARGO_BUILD_TARGET = rustTarget;
-            TARGET_CC = "${winCC}/bin/${winCC.targetPrefix}cc";
-            CARGO_BUILD_RUSTFLAGS = [
-              "-C"
-              "linker=${TARGET_CC}"
-            ];
-
-            CC = "${winCC}/bin/${winCC.targetPrefix}cc";
-            LD = "${winCC}/bin/${winCC.targetPrefix}cc";
-            postInstall = ''
-              cd "$out"/bin
-              for f in "$(ls)"; do
-                if ext="$(echo "$f" | grep -oP '\.[a-z]+$')"; then
-                  base="$(echo "$f" | cut -d. -f1)"
-                  mv "$f" "$base-${rustTarget}$ext"
-                else
-                  mv "$f" "$f-${rustTarget}"
-                fi
-              done
-            '';
           };
 
         mkCrossRustPackage =
           arch: packageName:
           let
-            rustTarget = arch2targets."${arch}".rustTarget;
-            crossTarget = arch2targets."${arch}".crossTarget;
-            pkgsCross = import nixpkgs {
-              system = system;
-              crossSystem.config = crossTarget;
-            };
-          in
-          let
+            inherit (arch2targets.${arch}) rustTarget pkgsCross depsBuildBuild;
             toolchain = fenixPkgs.combine [
               fenixPkgs.stable.rustc
               fenixPkgs.stable.cargo
@@ -258,6 +139,7 @@
             };
           in
           naersk-lib.buildPackage rec {
+            inherit depsBuildBuild;
             pname = packageName;
             cargoBuildOptions =
               x:
@@ -298,21 +180,22 @@
             LD = "${pkgsCross.stdenv.cc}/bin/${pkgsCross.stdenv.cc.targetPrefix}cc";
           };
 
-        mkRustPackages =
-          arch:
+        individualPackages =
           let
-            cargo-fslabscli = mkCrossRustPackage arch "cargo-fslabscli";
+            packageName = "cargo-fslabscli";
+            filteredTargets = lib.attrsets.filterAttrs (
+              k: _: (k == system || isDarwin || (!lib.strings.hasInfix "darwin" k))
+            ) arch2targets;
           in
-          {
-            "cargo-fslabscli-${arch}" = cargo-fslabscli;
-          };
-        individualPackages = mkRustPackages "x86_64-linux";
-        # // mkRustPackages "aarch64-linux"
-        # // mkRustPackages "aarch64-darwin"
-        # // {
-        # cargo-fslabscli-win64 = mkWin64RustPackage "cargo-fslabscli";
-        # cargo-fslabscli-win32 = mkWin32RustPackage "cargo-fslabscli";
-        # };
+          lib.attrsets.mapAttrs' (
+            arch: _:
+            let
+              shouldCross = arch != system;
+            in
+            lib.nameValuePair (packageName + "-" + arch) (
+              if shouldCross then (mkCrossRustPackage arch packageName) else (mkRustPackage packageName)
+            )
+          ) filteredTargets;
 
         treefmt = treefmt-nix.lib.evalModule pkgs {
           projectRootFile = "flake.nix";
@@ -322,13 +205,12 @@
             rustfmt.enable = true;
           };
         };
-        fslabscli = mkRustPackage "cargo-fslabscli";
       in
       {
         formatter = treefmt.config.build.wrapper;
 
         packages = individualPackages // {
-          default = fslabscli;
+          default = individualPackages."cargo-fslabscli-${system}";
           release = pkgs.runCommand "release-binaries" { } ''
             mkdir -p "$out/bin"
             for pkg in ${
@@ -358,7 +240,7 @@
               }:
               {
                 packages = with pkgs; [
-                  fslabscli
+                  self.packages.${system}.default
                   updatecli
                   cargo-deny
                   xunit-viewer
@@ -367,13 +249,6 @@
                   nix.enable = true;
                   rust = {
                     enable = true;
-                    toolchain = {
-                      cargo = toolchain;
-                      clippy = toolchain;
-                      rust-analyzer = toolchain;
-                      rustc = toolchain;
-                      rustfmt = toolchain;
-                    };
                   };
                 };
 
@@ -391,7 +266,7 @@
                   EOF
                   echo
                   echo Repository:
-                  echo  - https://github.com/orica-libs/orepro-infrastructure
+                  echo  - https://github.com/ForesightMiningSoftwareCorporation/fslabscli
                   echo ------------------------------------------------------------------
                   echo
                 '';
